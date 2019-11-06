0330b77e2e329ebfed6a28478b7bc465ef6c28a2
mrponcho <email@johannesbarth.com>
2019-11-06 17:01:32 +0100

    use styled from material ui in paper

--- components/StyledPaper/index.js
+++ components/StyledPaper/index.js
@@ -1,20 +1,16 @@
-import styled from 'styled-components';
+import { styled } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 
-const StyledPaper = styled(Paper)`
-  &&& {
-    display: block;
-    padding: 18px;
-    cursor: pointer;
-    transition: all .2s ease-in-out;
-    background: ${props => props.background};
-    color: ${props => props.color};
-
-      &:hover {
-        transform: scale(1.1);
-      }
-    }
-  }
-`;
+const StyledPaper = styled(Paper)(({ background, color }) => ({
+  display: 'block',
+  padding: 18,
+  cursor: 'pointer',
+  transition: 'all .2s ease-in-out',
+  background,
+  color,
+  '&:hover': {
+    transform: 'scale(1.1)',
+  },
+}));
 
 export default StyledPaper;
